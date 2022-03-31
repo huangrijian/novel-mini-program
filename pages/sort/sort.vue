ef84130fd9221cc1b8c8576fafbb030123ae7113
黄日健 <923691372@qq.com>
2022-03-31 09:50:22 +0800

    chore:分类模块右侧

--- pages/sort/sort.vue
+++ pages/sort/sort.vue
@@ -3,23 +3,51 @@
 		<view class="cal" :style="{height: `${statusBarHeight}px`}"></view>
 		<search-box class="mx-2 cal"></search-box>
 		<view class="cal" style="height: 20rpx;"></view>
-		
+
 		<view class="flex" style="background-color: #f0f3f8;">
 			<!-- 左侧 -->
-			<scroll-view scroll-y class="font text-light-black" style="width: 180rpx;padding: 0 20rpx;" :style="{height: `${calHeight}rpx`}">
-				123
+			<scroll-view scroll-y class="font text-light-black" style="width: 180rpx;padding: 0 20rpx;"
+				:style="{height: `${calHeight}rpx`}">
 				<block v-for="(item, index) in leftListRes" :key="index">
-					<view class="animated faster" :class="leftIndex === index ? 'selected pulse font-weight-bold':''" style="height: 150rpx;line-height: 150rpx;"
-					 @tap="leftToright(index)">{{item}}</view>
-					 
+					<view class="animated faster" :class="leftIndex === index ? 'selected pulse font-weight-bold':''"
+						style="height: 150rpx;line-height: 150rpx;" @tap="leftToright(index)">{{item}}</view>
 				</block>
+			</scroll-view>
+
+			<!-- 右侧 -->
+			<scroll-view scroll-y :scroll-into-view="rightIndex" scroll-with-animation
+				:style="{height: `${calHeight}rpx`}" @scroll="rightToleft">
+				<block v-for="(item, index) in rightListRes" :key="index">
+					<view style="height: 250rpx;margin-bottom: 70rpx;" :id="`right${index}`">
+
+
+						<!-- 顶部标题 -->
+						<view class="text-center" style="height: 80rpx;line-height: 80rpx;">
+							<text class="mr-1">{{item.text}}</text>
+							<my-icon iconId="icon-youjiantou" iconSize="40"></my-icon>
+						</view>
+
+
+						<!-- 底部内容 -->
+						<view class="flex flex-wrap bg-white rounded font" style="height: 170rpx;">
+							<block v-for="(mitem, mindex) in item.content" :key="mindex">
+								<view class="flex align-center justify-center" style="width: calc(100% / 3);">{{mitem}}
+								</view>
+							</block>
+						</view>
+					</view>
+				</block>
+				<!-- 占位 -->
+				<view :style="{height: `${calHeight - 320}rpx`}"></view>
 			</scroll-view>
 		</view>
 	</view>
 </template>
 
 <script>
-	import {sortResources as resources} from './sortResources';
+	import {
+		sortResources as resources
+	} from './sortResources';
 	import $U from '@/common/unit';
 	export default {
 		data() {
